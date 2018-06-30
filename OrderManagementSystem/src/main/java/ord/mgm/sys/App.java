9af9ed7584a62af7753575bc66c678587e9abf90
priyaksingh <priyaksingh@gmail.com>
2018-06-30 13:00:48 +0100

    initial project setup

---
+++ OrderManagementSystem/src/main/java/ord/mgm/sys/App.java
@@ -0,0 +1,13 @@
+package ord.mgm.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
